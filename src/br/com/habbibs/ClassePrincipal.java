e056d722c034ff60e0fc33cad49f53d0ae4543d7
Danilo <danio.germano@etec.sp.gov.br>
2018-11-23 20:24:43 -0200

    ClassePrincipal - Novo método criado

---
+++ src/br/com/habbibs/ClassePrincipal.java
@@ -0,0 +1,8 @@
+package br.com.habbibs;
+
+public class ClassePrincipal {
+    
+    public static void main(String[]args){
+        System.out.println("Novo Método criado; pronto para uso");
+    }
+}
